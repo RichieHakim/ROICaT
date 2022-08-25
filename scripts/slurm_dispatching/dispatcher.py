d002fd373a9b9f7eb020164b1146031e9e495224
RichieHakim <RichHakim@gmail.com>
2022-08-25 14:06:17 -0400

    playing with stuff to get it to run on o2

--- scripts/slurm_dispatching/dispatcher.py
+++ scripts/slurm_dispatching/dispatcher.py
@@ -76,7 +76,7 @@ params_template = {
     },
     'ROInet': {
         'device': 'cuda:0',  ## default: 'cuda:0'. Device to use for ROInet. Recommend using a GPU.
-        'dir_networkFiles': '/home/rich/Downloads/ROInet',  ## local directory where network files are stored
+        'dir_networkFiles': dir_save,  ## local directory where network files are stored
         'download_from_gDrive': 'check_local_first',  ## default: 'check_local_first'. Whether to download the network files from Google Drive or to use the local files.
         'gDriveID': '1FCcPZUuOR7xG-hdO6Ei6mx8YnKysVsa8',  ## default: '1FCcPZUuOR7xG-hdO6Ei6mx8YnKysVsa8'. Google Drive ID of the network files.
         'verbose': True,  ## default: True. Whether to print out ROInet information.
