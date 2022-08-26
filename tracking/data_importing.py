51e42719b451374c53077e7a7e3984b4949d64a5
RichieHakim <RichHakim@gmail.com>
2022-08-26 02:05:59 -0400

    playing with stuff to get it to run on o2

--- tracking/data_importing.py
+++ tracking/data_importing.py
@@ -18,7 +18,8 @@ class Data_suite2p:
         paths_statFiles,
         paths_opsFiles=None,
         um_per_pixel=1.0,
-        verbose=True
+        new_or_old_suite2p='new',
+        verbose=True 
     ):
         """
         Initializes the class for importing spatial footprints.
@@ -48,7 +49,14 @@ class Data_suite2p:
 
         self.um_per_pixel = um_per_pixel
 
+        self._new_or_old_suite2p = new_or_old_suite2p
+
         self._verbose = verbose
+
+        ## shifts are applied to convert the 'old' matlab version of suite2p indexing (where there is an offset and its 1-indexed)
+        self._shifts = [
+            np.array([op['yrange'].min()-1, op['xrange'].min()-1], dtype=np.uint64) for op in [np.load(path, allow_pickle=True)[()] for path in self.paths_ops]
+        ] if self._new_or_old_suite2p == 'old' else [np.array([0,0], dtype=np.uint64)]*len(self.statFiles)
 
 
     def import_statFiles(self):
@@ -177,8 +185,8 @@ class Data_suite2p:
         assert out_height_width[0]%2 == 0 and out_height_width[1]%2 == 0 , "'out_height_width' must be list of 2 EVEN integers"
         assert max_footprint_width%2 != 0 , "'max_footprint_width' must be odd"
 
-        self._out_height_width = out_height_width
-        self._max_footprint_width = max_footprint_width
+        self._out_height_width = np.uint64(out_height_width)
+        self._max_footprint_width = np.uint64(max_footprint_width)
 
         statFiles = self.import_statFiles() if self.statFiles is None else self.statFiles
 
@@ -217,6 +225,9 @@ class Data_suite2p:
             workers (int):
                 Number of workers to use for multiprocessing.
                 Set to -1. Note that this will use more memory.
+            new_or_old_suite2p (str):
+                'new': Python versions of Suite2p
+                'old': Matlab versions of Suite2p
 
         Returns:
             sf (list):
@@ -239,7 +250,7 @@ class Data_suite2p:
         if workers != 1:
             self.spatialFootprints = helpers.simple_multiprocessing(
                 _helper_populate_sf, 
-                (self.n_roi, [frame_height_width]*n, statFiles, [dtype]*n, [isInt]*n),
+                (self.n_roi, [frame_height_width]*n, statFiles, [dtype]*n, [isInt]*n, self._shifts),
                 workers=mp.cpu_count()
             )
         else:
@@ -250,6 +261,7 @@ class Data_suite2p:
                     stat=statFiles[ii],
                     dtype=dtype,
                     isInt=isInt,
+                    shifts=self._shifts[ii]
                 ) for ii in tqdm(range(n), mininterval=60)]
 
         self.sessionID_concat = np.vstack([np.array([helpers.idx2bool(i_sesh, length=len(self.spatialFootprints))]*sesh.shape[0]) for i_sesh, sesh in enumerate(self.spatialFootprints)])
@@ -279,25 +291,25 @@ class Data_suite2p:
 
         # sf_big: 'spatial footprints' prior to cropping. sf is after cropping
         sf_big_width = self._max_footprint_width # make odd number
-        sf_big_mid = sf_big_width // 2
+        sf_big_mid = np.uint64(sf_big_width // 2)
 
         sf_all_list = []
-        for stat in tqdm(statFiles, mininterval=60):
+        for ii, stat in tqdm(enumerate(statFiles), mininterval=60):
             if type(stat) is str or type(stat) is Path:
                 stat = np.load(stat, allow_pickle=True)
             n_roi = stat.shape[0]
 
             sf_big = np.zeros((n_roi, sf_big_width, sf_big_width))
             for ii in range(n_roi):
-                yIdx = stat[ii]['ypix'] - np.int16(stat[ii]['med'][0]) + sf_big_mid
-                xIdx = stat[ii]['xpix'] - np.int16(stat[ii]['med'][1]) + sf_big_mid
+                yIdx = np.array(stat[ii]['ypix'], dtype=np.uint64) - np.int64(stat[ii]['med'][0]) + sf_big_mid
+                xIdx = np.array(stat[ii]['xpix'], dtype=np.uint64) - np.int64(stat[ii]['med'][1]) + sf_big_mid
                 if np.any(yIdx < 0) or np.any(xIdx < 0) or np.any(yIdx >= sf_big_width) or np.any(xIdx >= sf_big_width):
                     raise IndexError(f"RH ERROR: Spatial footprint is out of bounds. Increase max_footprint_width.")
-                sf_big[ii][yIdx, xIdx] = stat[ii]['lam'] # (dim0: ROI#) (dim1: y pix) (dim2: x pix)
+                sf_big[ii][np.uint64(yIdx), np.uint64(xIdx)] = stat[ii]['lam'] # (dim0: ROI#) (dim1: y pix) (dim2: x pix)
 
             sf = sf_big[:,  
-                        sf_big_mid - self._out_height_width[0]//2:sf_big_mid + self._out_height_width[0]//2,
-                        sf_big_mid - self._out_height_width[1]//2:sf_big_mid + self._out_height_width[1]//2]
+                        sf_big_mid - np.uint64(self._out_height_width[0]//2) : sf_big_mid + np.uint64(self._out_height_width[0]//2),
+                        sf_big_mid - np.uint64(self._out_height_width[1]//2) : sf_big_mid + np.uint64(self._out_height_width[1]//2)]
 
             sf_all_list.append(sf)
 
@@ -330,7 +342,7 @@ def fix_paths(paths):
     return paths_files
 
 
-def _helper_populate_sf(n_roi, frame_height_width, stat, dtype, isInt):
+def _helper_populate_sf(n_roi, frame_height_width, stat, dtype, isInt, shifts=(0,0)):
     """
     Helper function for populate_sf.
     Populates a sparse array with the spatial footprints from ROIs
@@ -347,6 +359,8 @@ def _helper_populate_sf(n_roi, frame_height_width, stat, dtype, isInt):
             lam = dtype(lam / lam.sum() * np.iinfo(dtype).max)
         else:
             lam = lam / lam.sum()
-        sf[jj, roi['ypix'], roi['xpix']] = lam
+        ypix = np.array(roi['ypix'], dtype=np.uint64) + shifts[0]
+        xpix = np.array(roi['xpix'], dtype=np.uint64) + shifts[1]
+        sf[jj, ypix, xpix] = lam
 
     return scipy.sparse.csr_matrix(sf.reshape(sf.shape[0], -1))
