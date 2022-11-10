Updates for classification sample weighting and csr vs coo fix

roicat/classification/pipeline.py
@@ -71,7 +71,7 @@ def fit_pipe(feat_train, labels_train, preproc_init, classify, preproc_refit=Tru
         preproc.fit(feat_train, labels_train)
 
     try:
-        if balance_sample_size:
+        if balance_sample_weights:
             classify.fit(preproc.transform(feat_train), labels_train, sample_weight=get_balanced_sample_weights(labels_train.astype(np.int32)))
         else:
             classify.fit(preproc.transform(feat_train), labels_train)
@@ -137,3 +137,20 @@ def fit_n_train(features_train, labels_train, preproc_init, classify_init, prepr
     pipe = fit_pipe(features_train_subset, labels_train_subset, preproc_init, classify_init, preproc_refit=preproc_refit)
     
     return pipe, n_train
+
+
+def get_balanced_sample_weights(labels):
+    """
+    Balances sample ways for classification
+    
+    RH/JZ 2022
+    
+    labels: np.array
+        Includes list of labels to balance the weights for classifier training
+    returns weights by samples
+    """
+    labels = labels.astype(np.int64)
+    vals, counts = np.unique(labels, return_counts=True)
+    weights = len(labels) / counts
+    sample_weights = weights[labels]
+    return sample_weights

roicat/data_importing.py
@@ -421,6 +421,18 @@ class Data_suite2p:
             raise ValueError('Only valid methods are "centroid" or "median"')
         return np.round(np.vstack([h_mean, w_mean])).astype(np.int64)
 
+    
+#     def drop_nan_rois(self):
+#         """
+#         Identifies all entries along the 0th dimension of self.statFiles that
+#         have any NaN value in any of their dimensions and removes
+#         those entries from both self.statFiles and self.labelFiles
+#         """
+#         idx_nne = [helpers.get_keep_nonnan_entries(sf) for sf in self.statFiles]
+#         self.statFiles = [sf[inne] for sf, inne in zip(self.statFiles, idx_nne)]
+#         self.labelFiles = [lf[inne] for lf, inne in zip(self.labelFiles, idx_nne)]
+#         return self.statFiles, self.labelFiles
+
 
 class Data_caiman:
     """
@@ -827,6 +839,6 @@ def _helper_populate_sf(n_roi, frame_height_width, stat, dtype, isInt, shifts=(0
         tmp_roi = scipy.sparse.csr_array((lam, (ypix, xpix)), shape=(frame_height_width[0], frame_height_width[1]))
         rois_to_stack.append(tmp_roi.reshape(1,-1))
 
-    return scipy.sparse.vstack(rois_to_stack)
+    return scipy.sparse.vstack(rois_to_stack).tocsr()
 
 

roicat/helpers.py
@@ -750,7 +750,7 @@ def get_keep_nonnan_entries(original_features):
         original_features (np.ndarray): 
             image values (images x height x width)
     """
-    has_nan = np.unique(np.where(np.isnan(original_features))[0])
+    has_nan = [np.unique(np.where(np.isnan(of))[0]) for of in original_features]
     return np.array([_ for _ in range(original_features.shape[0]) if _ not in has_nan])
 
 