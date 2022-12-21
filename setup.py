a30cefc4b1fba06ca58463f8547123143628c2da
RichieHakim <RichHakim@gmail.com>
2022-12-20 20:39:47 -0500

    custom install pip reqs

--- setup.py
+++ setup.py
@@ -2,11 +2,24 @@
 
 from distutils.core import setup
 
-## Get install dependencies from requirements.txt
-pip_deps = []
-with open('requirements_GPU.txt', 'r') as f:
-    for line in f.readlines():
-        pip_deps.append(line.strip())
+from pathlib import Path
+import os
+
+## Manually install dependencies from requirements.txt
+### This is a workaround for the fact that pip does not support
+### installing from a requirements.txt file with custom URLs
+### (e.g. torch --extra-index-url https://download.pytorch.org/whl/torch_stable.html)
+### 1. Find path to requirements.txt
+### 2. Call pip install -r requirements.txt
+path_reqs = Path(__file__) / 'requirements_GPU.txt'
+assert path_reqs.exists(), 'No requirements.txt file found!'
+os.system(f'pip install -r {path_reqs}')
+
+
+# pip_deps = []
+# with open('requirements_GPU.txt', 'r') as f:
+#     for line in f.readlines():
+#         pip_deps.append(line.strip())
 
 setup(
     name='roicat',
@@ -21,6 +34,6 @@ setup(
     license='LICENSE',
     description='A library for classifying and tracking ROIs.',
     long_description=open('README.md').read(),
-    install_requires=pip_deps,
+    # install_requires=pip_deps,
     url='https://github.com/RichieHakim/ROICaT',
 )
