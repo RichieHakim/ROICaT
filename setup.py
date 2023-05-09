d282cafb40762ed880c7ccc8bedc84f60e00d686
RichieHakim <RichHakim@gmail.com>
2023-05-09 19:23:04 -0400

    UPDATE scipy to 1.10.1, PIN torch versions, COMMENT OUT a UMAP test, CHANGE type checking from np.int to int, CHANGE importlib_metadata to importlib.metadata

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@
 
 from distutils.core import setup
 
-from pathlib import Path
-import os
-
-import sys
-import os
-
 ## Dependencies: core requirements
 deps_core = [
     "umap-learn==0.5.3",
@@ -21,7 +15,7 @@ deps_core = [
     # "pandas==2.0.1",
     "Pillow==9.2.0",
     "scikit_learn==1.1.2",
-    "scipy==1.8.1",
+    "scipy==1.10.1",
     "seaborn==0.11.2",
     "sparse==0.14.0",
     "tqdm==4.64.0",
@@ -29,31 +23,20 @@ deps_core = [
     "jupyter",
     "paramiko==2.12.0",
     # "pyyaml==5.4.1",
-    # "hdfdict==0.3.1",
     "optuna==3.0.1",
     # "joblib==1.2.0",
     # "Cython==0.29.32",
     "einops==0.6.0",
     "xxhash==3.1.0",
     "pytest==7.2.0",
-    
-    "torch",
-    "torchvision",
-    "torchaudio",
+
+    "torch==2.0.1",
+    "torchvision==0.15.2",
+    "torchaudio==2.0.2",
 ]
 ## Dependencies: latest versions of core requirements
 ### remove everything starting and after the first =,>,<,! sign
 deps_core_latest = [req.split('=')[0].split('>')[0].split('<')[0].split('!')[0] for req in deps_core]
-
-
-# ## Dependencies: torch CPU
-# deps_torchCPU = [
-#     "torch==1.12.1",
-#     "torchvision==0.13.1",
-#     "torchaudio==0.12.1",
-# ]
-# ## Dependencies: torch CPU, latest versions
-# deps_torchCPU_latest = [req.split('=')[0].split('>')[0].split('<')[0].split('!')[0] for req in deps_torchCPU]
 
 
 ## Get README.md
@@ -81,7 +64,5 @@ setup(
     extras_require={
         'core': deps_core,
         'core_latest': deps_core_latest,
-        # 'torchCPU': deps_torchCPU,
-        # 'torchCPU_latest': deps_torchCPU_latest,
     },
 )
