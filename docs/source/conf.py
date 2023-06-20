6875690a8dd478165fadcf9aec0269c439c5028c
RichieHakim <RichHakim@gmail.com>
2023-06-20 18:23:28 -0400

    add paths to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,6 +8,17 @@
 
 from pathlib import Path
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+
+
 ## Get version number
 _dir_parent = Path(__file__).parent.parent.parent
 
