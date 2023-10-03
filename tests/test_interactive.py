0f27de9f30f22660038b368989f244f5b8a99b87
gyu-heo <heo9412@gmail.com>
2023-10-03 17:22:16 -0400

    Added some sanity check for macOS env

--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import warnings
 import pytest
 import tempfile
+import requests
 
 import numpy as np
 import torch
@@ -84,67 +85,97 @@ def deploy_bokeh(instance):
     instance.add_root(hv_layout)
 
 
-def test_interactive_drawing():
-    warnings.warn("Interactive GUI Drawing Test is running. Please wait...")
-    ## Bokeh server deployment at http://localhost:5000
-    apps = {"/": Application(FunctionHandler(deploy_bokeh))}
-
-    warnings.warn("Deploy Bokeh server to localhost:5000...")
-    ## Let it run in the background so that the test can continue
-    server_process = mp.Process(target=start_server, args=(apps,))
-    server_process.start()
-
-    warnings.warn("Setup chrome webdriver...")
-    service = Service()
-    chrome_options = Options()
-    chrome_options.add_argument("--window-size=1280,1280")
-    ## For local testing, just comment out the headless options.
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-
-    ## if you are on latest version say selenium v4.6.0 or higher, you don't have to use third party library such as WebDriverManager
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    warnings.warn("Get to the Bokeh server...")
-    driver.get("http://localhost:5000/")
-    wait = WebDriverWait(driver, 10)
+def check_server():
     try:
-        element = wait.until(EC.presence_of_element_located((By.XPATH, "//*")))
-        warnings.warn("Found Bokeh drawing element!")
+        response = requests.get("http://localhost:5000")
+        if response.status_code == 200:
+            warnings.warn("Server is up and running!")
+        else:
+            warnings.warn(f"Server responded with status code: {response.status_code}")
+    except requests.ConnectionError:
+        warnings.warn("Cannot connect to the server!")
+
+
+def test_interactive_drawing():
+    try:
+        warnings.warn("Interactive GUI Drawing Test is running. Please wait...")
+        ## Bokeh server deployment at http://localhost:5000
+        apps = {"/": Application(FunctionHandler(deploy_bokeh))}
+
+        warnings.warn("Deploy Bokeh server to localhost:5000...")
+        ## Let it run in the background so that the test can continue
+        server_process = mp.Process(target=start_server, args=(apps,))
+        server_process.start()
+
+        ## Check if the server is up and running
+        warnings.warn("Check if Bokeh server is up and running...")
+        check_server()
+
+        warnings.warn("Setup chrome webdriver...")
+        service = Service()
+        chrome_options = Options()
+        chrome_options.add_argument("--window-size=1280,1280")
+        ## For local testing, just comment out the headless options.
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+
+        ## if you are on latest version say selenium v4.6.0 or higher, you don't have to use third party library such as WebDriverManager
+        warnings.warn("Driver parameter sanity check...")
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        capabilities = driver.capabilities
+        warnings.warn("Browser Name: {}".format(capabilities.get("browserName")))
+        warnings.warn("Browser Version: {}".format(capabilities.get("browserVersion")))
+        warnings.warn("Platform Name: {}".format(capabilities.get("platformName")))
+        warnings.warn(
+            "Chrome Driver Version: {}".format(
+                capabilities.get("chrome").get("chromedriverVersion")
+            )
+        )
+
+        warnings.warn("Get to the Bokeh server...")
+        driver.get("http://localhost:5000/")
+        wait = WebDriverWait(driver, 10)
+        warnings.warn("Found the Bokeh server, locate drawing Bokeh element...")
+        try:
+            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*")))
+            warnings.warn("Found Bokeh drawing element!")
+        except Exception as e:
+            warnings.warn(f"Failed to locate element: {str(e)}")
+
+        ## Create movement set
+        size = element.size
+        width, height = size["width"], size["height"]
+
+        ## Move to the center of the element
+        warnings.warn("Start mouse movement...")
+        actions = ActionChains(driver)
+        actions.move_to_element(element)
+        actions.click_and_hold()
+
+        ## Draw!
+        actions.move_by_offset(
+            int(width / 2), int(0)
+        )  ## Move from center to midpoint of right edge
+        actions.move_by_offset(
+            int(0), int(-height / 2)
+        )  ## Move from midpoint of right edge to top right corner
+        actions.move_by_offset(
+            int(-width / 2), int(0)
+        )  ## Move from top right corner to midpoint of top edge
+        actions.release()
+        actions.perform()
+
+        warnings.warn("Mouse movement done! Detach Selenium from Bokeh server...")
+        driver.quit()
+
+        warnings.warn("Test if indices are correctly saved...")
+        indices = get_indices()
+        assert indices == [3]
     except Exception as e:
-        warnings.warn(f"Failed to locate element: {str(e)}")
-
-    ## Create movement set
-    size = element.size
-    width, height = size["width"], size["height"]
-
-    ## Move to the center of the element
-    warnings.warn("Start mouse movement...")
-    actions = ActionChains(driver)
-    actions.move_to_element(element)
-    actions.click_and_hold()
-
-    ## Draw!
-    actions.move_by_offset(
-        int(width / 2), int(0)
-    )  ## Move from center to midpoint of right edge
-    actions.move_by_offset(
-        int(0), int(-height / 2)
-    )  ## Move from midpoint of right edge to top right corner
-    actions.move_by_offset(
-        int(-width / 2), int(0)
-    )  ## Move from top right corner to midpoint of top edge
-    actions.release()
-    actions.perform()
-
-    warnings.warn("Mouse movement done! Detach Selenium from Bokeh server...")
-    driver.quit()
-
-    warnings.warn("Test if indices are correctly saved...")
-    indices = get_indices()
-    assert indices == [3]
-
-    warnings.warn("Test is done. Cleaning up...")
-    server_process.terminate()
-    server_process.join()
-    warnings.warn("Test is done. Cleaning up done.")
+        warnings.warn(f"Test failed: {str(e)}")
+        raise e
+    finally:
+        warnings.warn("Test is done. Cleaning up...")
+        server_process.terminate()
+        server_process.join()
+        warnings.warn("Test is done. Cleaning up done.")
