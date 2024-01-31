6b6935d088cc62b44b146bb389f220012a7c91db
RichieHakim <RichHakim@gmail.com>
2024-01-31 16:24:46 -0500

    Remove unused commented code from tests package

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# from os.path import dirname, basename, isfile, join
-# import glob
-# modules = glob.glob(join(dirname(__file__), "*.py"))
-# __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
-
-# for pkg in __all__:
-#     exec('from . import ' + pkg)
-
-# del dirname, basename, isfile, join, glob, modules, pkg
